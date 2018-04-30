3d4671fef5586a9023f44a037130f239eba0f7ba
Julioher <jahc-2012@hotmail.com>
2018-04-30 12:02:29 -0600

    segundo cambio por Julio en github

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ class Hello {
   public static void main(String[] args) {
     System.out.println("Hola mundo");
     System.out.println("Esta es una modificación hecha por Julio");
+    System.out.println("Esta es un cambio hecho en github");
   }
 }
